8f64e6b6f67f91bbe501ee706e6b2bd8133ad423
dioptric233 <141849074+dioptric233@users.noreply.github.com>
2023-10-07 15:05:58 +0800

    Add files via upload

---
+++ demo 3.0.1/src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+localStorage.setItem("login", String(true));
+import myAside from './components/aside.vue';
+</script>
+
+<template>
+  <div class="container">
+    <el-aside width="150px" class="el-aside">
+      <my-aside></my-aside>
+    </el-aside>
+    <el-container class="view">
+      <el-main><router-view /></el-main>
+    </el-container>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+}
+
+.view {
+  place-items: center
+}
+
+.el-aside {
+  order: -1;
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 200px;
+}
+</style>
---
+++ demo 3.0.1/src/pages/Choice.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import choice from "../components/choice.vue";
+</script>
+
+<template>
+    <el-row>
+        <el-col :span="16">
+            <choice></choice>
+        </el-col>
+    </el-row>
+</template>
+
+<style scoped></style>

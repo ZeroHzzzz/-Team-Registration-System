2cbd561f3c24d04b230ac95699b286183eb7e177
dioptric233 <141849074+dioptric233@users.noreply.github.com>
2023-10-07 16:14:14 +0800

    Delete demo 3.0.1 directory

--- demo 3.0.1/src/stores/index.ts
+++
@@ -1,5 +0,0 @@
-import { createPinia } from "pinia";
-import PiniaPluginPersistedstate from "pinia-plugin-persistedstate";
-const pinia = createPinia();
-pinia.use(PiniaPluginPersistedstate);
-export default pinia
